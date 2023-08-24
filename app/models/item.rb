64bf341d705e26c11e740c4c7aa6ef378aea7b45
shinya0117 <bbshin0117@gmail.com>
2023-08-25 00:04:05 +0900

    単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,23 +7,21 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :sipping_day
 
-  validates :content, presence: true, unless: :was_attached?
+  validates :item_image, presence: true, unless: :was_attached?
   
   def was_attached?
     self.item_image.attached?
   end
 
-  validates :item_name, presence:true
-  validates :item_info, presence:true
+  validates :item_name, presence:true, length: { minimum: 1, maximum: 40 }
+  validates :item_info, presence:true, length: { minimum: 1, maximum: 1000 }
   validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :item_sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :sipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :item_price, presence:true
-  validates :item_price, numericality: {
-    greater_than_or_equal_to: 300,
-    less_than_or_equal_to: 9_999_999,
-    message: "は300から9,999,999の間で設定してください。"
+  validates :item_price, presence: true, format: {
+    with: /\A(?:[3-9]\d{2}|[1-9]\d{4,6})\z/,
+    message: "は¥300から¥9,999,999の範囲で設定してください。"
   }
 end
