bab58822d3de99dd3a949412c556c5dbac6241dd
shinya0117 <bbshin0117@gmail.com>
2023-09-06 20:55:53 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,12 +25,10 @@ class ItemsController < ApplicationController
 
   def edit
     return unless current_user.id != @item.user_id
-
     redirect_to root_path
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
